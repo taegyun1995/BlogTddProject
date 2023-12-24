ac70d82950369ab63586c1c9dab283664012b7ea
taegyun1995 <needle12s@gmail.com>
2023-12-24 15:44:40 +0900

    feat: setting up before automating project deployments

---
+++ src/main/java/tdd/blog/health/HealthCheckController.java
@@ -0,0 +1,14 @@
+package tdd.blog.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "ok";
+    }
+
+}
