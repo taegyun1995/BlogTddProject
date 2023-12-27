a70495a9deff127376b373e05037f45dd3050bb7
taegyun1995 <needle12s@gmail.com>
2023-12-27 23:36:00 +0900

    deploy: create health check api

--- src/main/java/tdd/blogProject/health/HealthCheckController.java
+++ src/main/java/tdd/blogProject/health/HealthCheckController.java
@@ -1,4 +1,4 @@
-package tdd.blog.health;
+package tdd.blogProject.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
