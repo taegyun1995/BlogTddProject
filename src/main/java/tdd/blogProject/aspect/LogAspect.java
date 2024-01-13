fc536495cbb73479b7c24e53a8e5f3c8d6c41ed3
taegyun1995 <needle12s@gmail.com>
2024-01-13 17:04:27 +0900

    feat: log aop setting

--- src/main/java/tdd/blogProject/aspect/LogAspect.java
+++ src/main/java/tdd/blogProject/aspect/LogAspect.java
@@ -2,9 +2,9 @@ package tdd.blogProject.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,14 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Before("execution(* tdd.blogProject.blog..*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        log.info("Before: " + joinPoint.getSignature().toShortString());
+    @Pointcut("execution(* tdd.blogProject..*.*(..))")
+    public void inBackendPackage() {
     }
 
-    @AfterReturning(pointcut = "execution(* tdd.blogProject.blog..*.*(..))", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        log.info("After Returning: " + joinPoint.getSignature().toShortString() + ", Result: " + result);
+    @AfterThrowing(pointcut = "inBackendPackage()", throwing = "ex")
+    public void handleException(JoinPoint joinPoint, Exception ex) {
+        log.error(
+                "An error occurred while executing method: {} " +
+                        "in class: {} " +
+                        "with arguments: {}",
+                joinPoint.getSignature().toShortString(),
+                joinPoint.getTarget().getClass().getName(),
+                joinPoint.getArgs(),
+                ex
+        );
     }
-
 }
