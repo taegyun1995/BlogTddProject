8066940137d8b172070de9866952abf4abb5a213
taegyun1995 <needle12s@gmail.com>
2024-01-14 23:22:11 +0900

    feat: implementing the blog controller api with tdd

--- src/test/java/tdd/blogProject/blog/adapter/in/web/BlogCreateControllerTest.java
+++ src/test/java/tdd/blogProject/blog/adapter/in/web/BlogCreateControllerTest.java
@@ -1,8 +1,8 @@
 package tdd.blogProject.blog.adapter.in.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,20 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import tdd.blogProject.advice.exception.ExceptionResponse;
-import tdd.blogProject.advice.success.SuccessResponse;
 import tdd.blogProject.blog.application.port.in.BlogCreateCommand;
 import tdd.blogProject.blog.application.port.in.BlogCreateUseCase;
 import tdd.blogProject.blog.domain.BlogTitle;
 import tdd.blogProject.user.domain.UserName;
 
+import java.util.Optional;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.filter.log.LogDetail.ALL;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.http.ContentType.JSON;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static tdd.blogProject.advice.success.SuccessCodeConst.BLOG_CREATE_SUCCESS;
+import static org.springframework.http.HttpStatus.*;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class BlogCreateControllerTest {
@@ -43,39 +44,52 @@ public class BlogCreateControllerTest {
     }
 
     @Test
-    @DisplayName("POST /api/v1/blog => (201)")
+    @DisplayName("POST /api/v1/blog => (201) Created")
     void testCreateBlog() throws Exception {
         BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
 
-        SuccessResponse response = new SuccessResponse(BLOG_CREATE_SUCCESS);
-
         given()
-                .contentType(APPLICATION_JSON_VALUE)
+                .contentType(JSON)
                 .body(objectMapper.writeValueAsString(command))
                 .when()
                 .post("/api/v1/blog")
                 .then()
-                .statusCode(201)
-                .log().ifValidationFails(ALL)
-                .body(equalTo(response.toJsonString()));
+                .statusCode(CREATED.value())
+                .log().ifValidationFails(ALL);
+
+        verify(useCase, times(1)).createBlog(any());
     }
 
-/*    @Test
-    @DisplayName("POST /api/v1/blog => (416)")
-    void testCreateBlogNegative() throws Exception {
-        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle(""), new UserName("Test UserName"));
-
+    @Test
+    @DisplayName("POST /api/v1/blog => (400) Bad Request")
+    public void testCreateBlogBadRequest() {
         given()
-                .contentType(APPLICATION_JSON_VALUE)
-                .body(objectMapper.writeValueAsString(command))
+                .contentType(JSON)
+                .body(Optional.class)
                 .when()
                 .post("/api/v1/blog")
                 .then()
-                .statusCode(416)
-                .log().ifValidationFails(ALL)
-                .body("status", equalTo(416))
-                .body("code", equalTo("LENGTH_REQUIRED_TITLE"))
-                .body("message", equalTo("블로그 제목은 1자 이상 20자 이하의 문자열이어야 합니다."));
-    }*/
+                .statusCode(BAD_REQUEST.value())
+                .log().ifValidationFails(ALL);
+
+        verify(useCase, times(0)).createBlog(any());
+    }
+
+    @Test
+    @DisplayName("POST /not_found => (404) Not Found")
+    public void testCreateBlogNotFound() throws JsonProcessingException {
+        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
+
+        given()
+                .contentType(JSON)
+                .body(objectMapper.writeValueAsString(command))
+                .when()
+                .post("/not_found")
+                .then()
+                .statusCode(NOT_FOUND.value())
+                .log().ifValidationFails(ALL);
+
+        verify(useCase, times(0)).createBlog(any());
+    }
 
 }
