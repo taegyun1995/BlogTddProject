1d99f70c5c4149fc541d8c3a9aa7fac5d7905c1b
taegyun1995 <needle12s@gmail.com>
2024-01-15 22:08:16 +0900

    feat: added negative case tests for blog creation controller content types

--- src/test/java/tdd/blogProject/blog/adapter/in/web/BlogCreateControllerTest.java
+++ src/test/java/tdd/blogProject/blog/adapter/in/web/BlogCreateControllerTest.java
@@ -3,6 +3,7 @@ package tdd.blogProject.blog.adapter.in.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,8 +22,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.http.ContentType.JSON;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.*;
 
@@ -30,7 +30,7 @@ import static org.springframework.http.HttpStatus.*;
 public class BlogCreateControllerTest {
 
     @MockBean
-    private BlogCreateUseCase useCase;
+    private BlogCreateUseCase sut;
 
     @LocalServerPort
     private int port;
@@ -57,7 +57,7 @@ public class BlogCreateControllerTest {
                 .statusCode(CREATED.value())
                 .log().ifValidationFails(ALL);
 
-        verify(useCase, times(1)).createBlog(any());
+        verify(sut, times(1)).createBlog(any());
     }
 
     @Test
@@ -72,7 +72,7 @@ public class BlogCreateControllerTest {
                 .statusCode(BAD_REQUEST.value())
                 .log().ifValidationFails(ALL);
 
-        verify(useCase, times(0)).createBlog(any());
+        verify(sut, times(0)).createBlog(any());
     }
 
     @Test
@@ -89,7 +89,78 @@ public class BlogCreateControllerTest {
                 .statusCode(NOT_FOUND.value())
                 .log().ifValidationFails(ALL);
 
-        verify(useCase, times(0)).createBlog(any());
+        verify(sut, times(0)).createBlog(any());
+    }
+
+    @Test
+    @DisplayName("POST /api/v1/blog with TEXT content type => (415) Unsupported Media Type")
+    void testCreateBlogWithInvalidContentType() throws Exception {
+        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
+
+        given()
+                .contentType(ContentType.TEXT)
+                .body(objectMapper.writeValueAsString(command))
+                .when()
+                .post("/api/v1/blog")
+                .then()
+                .statusCode(UNSUPPORTED_MEDIA_TYPE.value())
+                .log().ifValidationFails(ALL);
+
+        verify(sut, times(0)).createBlog(any());
+    }
+
+    @Test
+    @DisplayName("POST /api/v1/blog with XML content type => (415) Unsupported Media Type")
+    void testCreateBlogWithXmlContentType() throws Exception {
+        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
+
+        given()
+                .contentType(ContentType.XML)
+                .body(objectMapper.writeValueAsString(command))
+                .when()
+                .post("/api/v1/blog")
+                .then()
+                .statusCode(UNSUPPORTED_MEDIA_TYPE.value())
+                .log().ifValidationFails(ALL);
+
+        verify(sut, times(0)).createBlog(any());
+    }
+
+    @Test
+    @DisplayName("POST /api/v1/blog with HTML content type => (415) Unsupported Media Type")
+    void testCreateBlogWithHtmlContentType() throws Exception {
+        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
+
+        given()
+                .contentType(ContentType.HTML)
+                .body(objectMapper.writeValueAsString(command))
+                .when()
+                .post("/api/v1/blog")
+                .then()
+                .statusCode(UNSUPPORTED_MEDIA_TYPE.value())
+                .log().ifValidationFails(ALL);
+
+        verify(sut, times(0)).createBlog(any());
+    }
+
+
+    @Test
+    @DisplayName("POST /api/v1/blog => (500) Internal Server Error")
+    public void testCreateBlogInternalServerError() throws JsonProcessingException {
+        BlogCreateCommand command = new BlogCreateCommand(new BlogTitle("Test Blog Title"), new UserName("Test UserName"));
+
+        doThrow(new RuntimeException("Internal Server Error")).when(sut).createBlog(any());
+
+        given()
+                .contentType(JSON)
+                .body(objectMapper.writeValueAsString(command))
+                .when()
+                .post("/api/v1/blog")
+                .then()
+                .statusCode(INTERNAL_SERVER_ERROR.value())
+                .log().ifValidationFails(ALL);
+
+        verify(sut, times(1)).createBlog(any());
     }
 
 }
