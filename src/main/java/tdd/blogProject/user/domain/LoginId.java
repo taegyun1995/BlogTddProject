0908fd4ce2a0477fbaab5fc7168df6a745f3fae0
taegyun1995 <needle12s@gmail.com>
2024-01-25 23:23:04 +0900

    fix: add login username domain validation

--- src/main/java/tdd/blogProject/user/domain/LoginId.java
+++ src/main/java/tdd/blogProject/user/domain/LoginId.java
@@ -1,8 +1,15 @@
 package tdd.blogProject.user.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import tdd.blogProject.advice.exception.custom.BadRequestException;
+import tdd.blogProject.advice.exception.custom.LengthRequiredException;
+import tdd.blogProject.user.application.port.in.UserSignUpCommand;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -11,10 +18,40 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 public class LoginId {
 
+    private static final Pattern PATTERN = Pattern.compile("^.{1,15}$");
+
+    @Column(name = "login_id")
     private String value;
 
     public LoginId(String value) {
         this.value = value;
+    }
+
+    public static LoginId of(UserSignUpCommand command) {
+        validate(command.getLoginId().value);
+        return new LoginId(command.getLoginId().value);
+    }
+
+    private static void validate(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new BadRequestException(null);
+        }
+        if (!PATTERN.matcher(value).matches()) {
+            throw new LengthRequiredException(null);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginId loginId = (LoginId) o;
+        return Objects.equals(getValue(), loginId.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 
 }
